88c44f689a5cf591e18ef0e7a13de34f4ce7c7c3
Narahari <anarahari@gmail.com>
2014-10-25 23:19:04 +0800

    added a sample of interacting with redis over raw socket

---
+++ redis_socket.py
@@ -0,0 +1,14 @@
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM,socket.IPPROTO_TCP)
+sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
+sock.connect(('127.0.0.1',6379))
+
+print sock.family
+print sock.type
+print sock.proto
+
+sock.send('get a\n')
+data = sock.recv(1024)
+
+print data
