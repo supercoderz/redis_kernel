bfd9d9f58cd99b6f912931581c7ebfee85fb23b0
Narahari Allamraju <anarahari@gmail.com>
2014-10-25 20:43:51 +0800

    wip

--- redis_kernel.py
+++ redis_kernel.py
@@ -1,15 +1,15 @@
 from IPython.kernel.zmq.kernelbase import Kernel
-import redis
+import socket
 
 class RedisKernel(Kernel):
 	#these are required for the kernel to identify itself
 	implementation = 'redis_kernel'
 	implementation_version = '0.1'
 	language = 'redis'		  
-	
-	#our redis instance
-	redis_db = None
 
+	#the database connection
+	redis_socket = None
+	
 	#required for the kernel
 	@property
 	def language_version(self):
@@ -26,17 +26,25 @@ class RedisKernel(Kernel):
 		self.start_redis()
 		
 	def start_redis(self):
-		if self.redis_db is None:
-			redis_db = redis.Redis()
+		if self.redis_socket is None:
+			self.redis_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.redis_socket.connect(('localhost',6379))
 	
 	#the core of the kernel where the work happens
 	def do_execute(self, code, silent, store_history=True, user_expressions=None,
 					   allow_stdin=False):
 		
+		data = ''
+		print code
+		try:
+			self.redis_socket.send(code)
+			data = self.redis_socket.recv(1024)
+		except:
+			pass
 		#if you want to send output
 		if not silent:
 			#create the output here
-			stream_content = {'name': 'stdout', 'data':code}
+			stream_content = {'name': 'stdout', 'data':data}
 			self.send_response(self.iopub_socket, 'stream', stream_content)
 
 		#must return this always
