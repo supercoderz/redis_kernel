ffee8cb274d978d687a1353f3f6bceb28f32709a
Narahari <anarahari@gmail.com>
2014-11-05 21:19:55 +0800

    trying to fix tests for python 3

--- test_redis_parser.py
+++ test_redis_parser.py
@@ -11,25 +11,25 @@ class TestRedisParser(object):
 	
 	def test_parse_simple_string(self):
 		out = RedisParser('+Hello\r\n')
-		assert out._repr_text_() == "'Hello'"
-		assert out._repr_html_() == "<p>'Hello'</p>"
+		assert out._repr_text_() == str("'Hello'")
+		assert out._repr_html_() == str("<p>'Hello'</p>")
 
 	def test_parse_error(self):
 		out = RedisParser('-Error\r\n')
-		assert out._repr_text_() == "'Error'"
-		assert out._repr_html_() == "<p>'Error'</p>"
+		assert out._repr_text_() == str("'Error'")
+		assert out._repr_html_() == str("<p>'Error'</p>")
 
 	def test_parse_integer(self):
 		out = RedisParser(':1000\r\n')
-		assert out._repr_text_() == "'1000'"
-		assert out._repr_html_() == "<p>'1000'</p>"
+		assert out._repr_text_() == str("'1000'")
+		assert out._repr_html_() == str("<p>'1000'</p>")
 
 	def test_parse_bulk_string(self):
 		out = RedisParser('$5\r\nHello\r\n')
-		assert out._repr_text_() == "'Hello'"
-		assert out._repr_html_() == "<p>'Hello'</p>"
+		assert out._repr_text_() == str("'Hello'")
+		assert out._repr_html_() == str("<p>'Hello'</p>")
 
 	def test_parse_array(self):
 		out = RedisParser('*2\r\n$5\r\nHello\r\n$2\r\nHi\r\n')
-		assert out._repr_text_() == "['Hello', 'Hi']"
-		assert out._repr_html_() == "<p>['Hello', 'Hi']</p>"
+		assert out._repr_text_() == str("['Hello', 'Hi']")
+		assert out._repr_html_() == str("<p>['Hello', 'Hi']</p>")
